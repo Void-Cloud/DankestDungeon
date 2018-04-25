afd2bcb2e9112363aa444247f305ef995b37758a
Void-Cloud <36408441+Void-Cloud@users.noreply.github.com>
2018-04-25 15:18:20 +0300

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -4,8 +4,8 @@ USE dankestdungeon;
 
 CREATE TABLE Room
 (
-  RoomID INT NOT NULL AUTO_INCREMENT,
-  Encounter BOOLEAN NOT NULL, --Marks whether the room has a random encounter
+  RoomID INT NOT NULL,
+  Encounter BOOLEAN NOT NULL, #Marks whether the room has a random encounter
   Description VARCHAR(255) NOT NULL,
   Level INT NOT NULL,
   PRIMARY KEY (RoomID)
@@ -22,7 +22,7 @@ CREATE TABLE EnemyType
   Dialogue VARCHAR(255) NOT NULL,
   DeathDialogue VARCHAR(255) NOT NULL,
   Riddle VARCHAR(255) NOT NULL,
-  Unique BOOLEAN NOT NULL,
+  isUnique BOOLEAN NOT NULL,
   Money INT NOT NULL,
   PRIMARY KEY (EnemytypeID)
 );
@@ -85,13 +85,13 @@ CREATE TABLE Itemtype
   Movable BOOLEAN NOT NULL,
   Type VARCHAR(255) NOT NULL,
   Value INT NOT NULL,
-  Exists BOOLEAN NOT NULL,
+  Created BOOLEAN NOT NULL,
   PRIMARY KEY (ItemtypeID)
 );
 
 CREATE TABLE Can_cast
 (
-  EnemytypeID INT NOT NULL AUTO_INCREMENT,
+  EnemytypeID INT NOT NULL,
   MagicID INT NOT NULL,
   PRIMARY KEY (EnemytypeID, MagicID),
   FOREIGN KEY (EnemytypeID) REFERENCES EnemyType(EnemytypeID),
@@ -100,7 +100,7 @@ CREATE TABLE Can_cast
 
 CREATE TABLE Leads_to
 (
-  Direction VARCHAR(255) NOT NULL AUTO_INCREMENT,
+  Direction VARCHAR(255) NOT NULL,
   RoomID_1 INT NOT NULL,
   Leads_toRoomID_2 INT NOT NULL,
   PRIMARY KEY (RoomID_1, Leads_toRoomID_2),
@@ -135,3 +135,35 @@ CREATE TABLE Item
   FOREIGN KEY (MerchantID) REFERENCES Merchant(MerchantID),
   FOREIGN KEY (ItemtypeID) REFERENCES Itemtype(ItemtypeID)
 );
+
+#Ensimmäinen kerros ja huoneet
+
+INSERT INTO Room VALUES(1, 0,"Top Room", 1);
+INSERT INTO Room VALUES(2, 1,"Second row left", 1);
+INSERT INTO Room VALUES(3, 1,"Second row right - trap", 1);
+INSERT INTO Room VALUES(4, 1,"Third row middle", 1);
+INSERT INTO Room VALUES(5, 1,"Third row right", 1);
+INSERT INTO Room VALUES(6, 1,"Third row left - trap", 1);
+INSERT INTO Room VALUES(7, 1,"Fourth row middle left", 1);
+INSERT INTO Room VALUES(8, 1,"Fourth row further left - trap", 1);
+INSERT INTO Room VALUES(9, 0,"Fourth row middle right - sphinx", 1);
+INSERT INTO Room VALUES(10, 0,"Fourth row further right - BOSS ROOM", 1);
+
+INSERT INTO Leads_to VALUES("D",1,2);
+INSERT INTO Leads_to VALUES("U",2,1);
+INSERT INTO Leads_to VALUES("E",2,3);
+INSERT INTO Leads_to VALUES("W",3,2);
+INSERT INTO Leads_to VALUES("D",3,4);
+INSERT INTO Leads_to VALUES("U",4,3);
+INSERT INTO Leads_to VALUES("E",4,5);
+INSERT INTO Leads_to VALUES("W",5,4);
+INSERT INTO Leads_to VALUES("W",4,6);
+INSERT INTO Leads_to VALUES("E",6,4);
+INSERT INTO Leads_to VALUES("D",6,7);
+INSERT INTO Leads_to VALUES("U",7,6);
+INSERT INTO Leads_to VALUES("W",7,8);
+INSERT INTO Leads_to VALUES("E",8,7);
+INSERT INTO Leads_to VALUES("E",7,9);
+INSERT INTO Leads_to VALUES("W",9,7);
+INSERT INTO Leads_to VALUES("E",9,10);
+INSERT INTO Leads_to VALUES("W",10,9);
